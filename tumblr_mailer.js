8c2f955a1ba5657a4ec760bacb125772dc64d16f
Nicole <nsiegel2@gmail.com>
2015-10-28 18:24:34 -0400

    updated csvParse using forEach function

--- tumblr_mailer.js
+++ tumblr_mailer.js
@@ -1,20 +1,21 @@
 var fs = require('fs');
 
 var csvFile = fs.readFileSync("friend_list.csv","utf8");
+var emailTemplate = fs.readFileSync("email_template.html","utf8");
 
 var csvParse = function(csvFile) {
   var arrOfObj = [];
   var arr = csvFile.split('\n');
   arr = arr.slice(0, arr.length -1);
   var keys = arr.shift().split(',');
-  for (var i = 0; i < arr.length; i++) {
+  arr.forEach(function(contact) {
     var obj = {};
-    var contact = arr[i].split(',');
-    for (var j = 0; j < contact.length; j++) {
-      obj[keys[j]] = contact[j];
+    contact = contact.split(',');
+    for (var i = 0; i < contact.length; i++) {
+      obj[keys[i]] = contact[i];
     }
     arrOfObj.push(obj);
-  };
+  });
   return arrOfObj;
 };
 
