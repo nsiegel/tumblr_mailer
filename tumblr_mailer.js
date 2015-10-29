4291ce61d023246428ab694661b96874646ee00d
Nicole <nsiegel2@gmail.com>
2015-10-28 23:59:30 -0400

    added list of blog posts into rendered templates

--- tumblr_mailer.js
+++ tumblr_mailer.js
@@ -1,8 +1,18 @@
 var fs = require('fs');
 var ejs = require('ejs');
+var tumblr = require('tumblr.js');
+var client = tumblr.createClient({
+  consumer_key: '7tFQ92LU9LOWOsFlYnMGIfsFL081u0tgylZmcZAgPHuDvH4R9W',
+  consumer_secret: 'bsuIYK09nkfZM5CTkOI1Corv8y9e2GJgi4Ii1s54HOUj4bl3js',
+  token: 'NKJOeAFavDFTLcmsAsgXdg0zcwOB6Ce2u1cqFAdXCxPUFqVUSo',
+  token_secret: 'bWVzR5ynKzHSS3xrnbA6PFUB1sDqtvnwMu8NaVm1TWUFwr18dM'
+});
 
 var csvFile = fs.readFileSync("friend_list.csv","utf8");
 var emailTemplate = fs.readFileSync("email_template.ejs","utf8");
+
+var week = 604800; //seconds in a week
+var now = Date.now() / 1000; // current date in seconds
 
 var csvParse = function(csvFile) {
   var arrOfObj = [];
@@ -22,7 +32,18 @@ var csvParse = function(csvFile) {
 
 var contactList = csvParse(csvFile);
 
-contactList.forEach(function(contact) {
-  var personalizedEmail = ejs.render(emailTemplate, contact);
-  console.log(personalizedEmail);
+client.posts('nsiegel2.tumblr.com', function(err, blog){
+  var latestPosts = blog.posts.filter(function(post) {
+    if (post.timestamp > now - week) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  contactList.forEach(function(contact) {
+    contact.latestPosts = latestPosts;
+    var personalizedEmail = ejs.render(emailTemplate, contact);
+    console.log(personalizedEmail);
+  });
 });
